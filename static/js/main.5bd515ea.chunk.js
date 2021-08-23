c1def8fb408d59ccced75dcb1ca4e3f4367a1c13
Angel-Arceo <aalberto1812@gmail.com>
2021-08-23 17:40:00 -0500

    Updates

--- static/js/main.5bd515ea.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpfood-app"]=this["webpackJsonpfood-app"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var o=n(0),c=n.n(o),r=n(3),s=n.n(r),p=(n(8),n(1)),d=function(){return Object(p.jsx)("div",{className:"App",children:"Hello World"})};s.a.render(Object(p.jsx)(c.a.StrictMode,{children:Object(p.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.5bd515ea.chunk.js.map
