42025dd324ec361a3882e3018b2165e9039b7fd4
Luis Calderon <luis.calderon.lc2019@gmail.com>
2021-08-23 17:49:45 -0600

    refactor: use external-svg-loader instead img

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import "./Icon.css";
+import "external-svg-loader";
 
 const path = process.env.PUBLIC_URL.concat("/assets/icons/");
 
  const Icon = ({name, alt}) => {
     return (
-        <img className="icon" src={`${path}${name}.svg`} alt={alt}/>
+        <div className="icon">
+            
+        <svg
+        data-src={`${path}${name}.svg`}
+        fill="currentColor"
+        width="2.3em"
+        height="2.3em"
+        style={{
+          color: "var(--bg-secundary)"
+        }}
+        >
+        </svg>
+        </div>
     )
 };
 
