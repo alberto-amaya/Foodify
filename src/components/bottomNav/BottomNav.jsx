2036677f8fa3b1d0dd37f9fadb3a4fe9dc29b40c
Luis Calderon <luis.calderon.lc2019@gmail.com>
2021-08-23 18:01:07 -0600

    feat: add some icons and styles

--- src/components/bottomNav/BottomNav.jsx
+++ src/components/bottomNav/BottomNav.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import "./BottomNav.jsx";
+import Icon from '../icon/Icon.jsx';
+import "./BottomNav.css";
+
+
 
 export default function BottomNav() {
     return (
-        <nav>
+        <nav className="bottomnav" >
+            <ul>
+                <li>
+                    <Icon name="home" alt="home" active={true}/>
+                </li>
+            
+                <li>
+                     <Icon name="cutlery" alt="find recipes"/>
+                </li>
 
+                <li>
+                     <Icon name="user" alt="account"/>
+                </li>
+                <li>
+                     <Icon name="recipes" alt="my recipes"/>
+                </li>
+            </ul>
         </nav>
     )
 }
