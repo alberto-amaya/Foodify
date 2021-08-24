34d689738a25285a3b817582063a8c58276b741c
Luis Calderon <luis.calderon.lc2019@gmail.com>
2021-08-24 15:45:04 -0600

    feat: add useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url) {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const res = await fetch(url);
+                if (!res.ok) {
+                    throw {
+                        err: true,
+                        status: res.status,
+                        statusText: res.statusText ? res.statusText : "An error has ocurred",
+                    }
+                }
+                const data = await res.json();
+
+                setIsPending(false);
+                setData(data);
+                setError({ err: false })
+            } catch (e) {
+                setError(e);
+                setIsPending(true);
+            }
+        }
+        getData();
+    }, [url])
+
+    return {
+        data,
+        isPending,
+        error
+    }
+
+}
