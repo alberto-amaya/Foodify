7b356acfe4c28a9a32bac783362e7962a80e56d3
Luis Calderon <luis.calderon.lc2019@gmail.com>
2021-08-24 15:23:52 -0600

    refactor: change svg icon to background-img and add some styles

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useRef } from 'react';
+import Icon from '../iconComponent/Icon';
 import './Search.css';
 
 const Search = (props) => {
@@ -7,16 +8,13 @@ const Search = (props) => {
     const handleClick = () => {
         props.onSearch(inputSearch.current.value);
     }
+    const bgStyle={
+        backgroundImage: `url(${process.env.PUBLIC_URL}/assets/icons/search.svg)`,
+        
+    }
     return (
         <Fragment>
-            <input type="text" placeholder="Search" onChange={handleClick} ref={inputSearch} />
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#929298" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <circle cx="10" cy="10" r="7" />
-                    <line x1="21" y1="21" x2="15" y2="15" />
-                </svg>
-            </button>
+            <input style={bgStyle} type="text" placeholder="Search" onChange={handleClick} ref={inputSearch} />
         </Fragment>
     );
 }
