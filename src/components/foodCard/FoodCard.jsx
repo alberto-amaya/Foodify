1372080f5fb26ab28565422b5a6ad766f0f24b37
Angel-Arceo <aalberto1812@gmail.com>
2021-08-26 14:46:10 -0500

    Finishing the recipe card component

--- src/components/foodCard/FoodCard.jsx
+++ src/components/foodCard/FoodCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import './FoodCard.css';
+import { Link } from 'react-router-dom';
 
 const FoodCard = ({data}) => {
     const { title, image } = data;
--- src/components/recipeCard/RecipeCard.jsx
+++ src/components/recipeCard/RecipeCard.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './RecipeCard.css';
 
+import { Link } from 'react-router-dom';
 
-function RecipeCard() {
+const RecipeCard = (props) => {
+    const [ summary, setSummary ] = useState(props.summary); 
+
+    useEffect(() => setSummary(summary.replace(/(&nbsp;|<([^>]+)>)/ig, "")) , [summary]);
+
     return (
-        <article className="Recipe-Card">
-            Not created yet
-        </article>
+        <Link to='/recipe' className="Link">
+            <article className="Recipe-Card">
+                <div className="Recipe-Card--recipeImage">
+                    <img src={props.img} alt="recipe" />
+                </div>
+                <div className="Recipe-Card--recipeContent">
+                    <h2>{props.title.substr(0,50)+'..'}</h2>
+                    <p>{`${summary.substr(0,55)}...`}</p>
+                </div>
+            </article>
+        </Link>
     );
 }
 
