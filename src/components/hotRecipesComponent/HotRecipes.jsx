83f5a7c051d000660c24fa162240864d9dd7fd9c
Angel-Arceo <aalberto1812@gmail.com>
2021-08-25 23:45:31 -0500

    Uploading the hot recipes component

--- src/components/hotRecipesComponent/HotRecipes.jsx
+++ src/components/hotRecipesComponent/HotRecipes.jsx
@@ -1,9 +1,18 @@
 import React, { Fragment } from 'react';
+import './HotRecipes.css';
+
+import { Link } from 'react-router-dom';
+import Icon from '../iconComponent/Icon';
 
 const HotRecipes = () => {
     return (
         <Fragment>
-            <h1>Hot Recipes</h1>
+            <div className="main-hot-recipes--header">
+                <h1>Hot Recipes</h1>
+                <Link to="/" className="link">
+                    <p>More</p> <Icon name="next" width="27" height="27" color="#FFF" className="icon" />
+                </Link>
+            </div>
         </Fragment>
     );
 }
