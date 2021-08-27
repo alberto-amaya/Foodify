768b8e4a69be374c4a85d3f67583a6e4156eb4b8
David Ordás <davorpatech+git@gmail.com>
2021-08-27 18:44:25 +0200

    fix: ci error by "no-throw-literal" eslint rule
    On ci environments eslint warnings are promoted as errors
    
    Two approach:
    - Disable this rule  "no-throw-literal"
    - Apply OOP to not throw plain object errors.
    
    TODO: reuse refactor. extract utilities codeblocks like fetch class getters.

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -10,29 +10,113 @@ export default function useFetch(url) {
             try {
                 const res = await fetch(url);
                 if (!res.ok) {
-                    throw {
-                        err: true,
+                    /* eslint-disable-next-line no-throw-literal */
+                    throw new FetchError({
+                        //err: true, //promoted as getter
                         status: res.status,
                         statusText: res.statusText ? res.statusText : "An error has ocurred",
-                    }
+                    });
                 }
                 const data = await res.json();
 
                 setData(data);
                 setIsPending(false);
-                setError({ err: false })
+                setError({ err: false });
             } catch (e) {
                 setError(e);
                 setIsPending(true);
             }
         }
         getData();
-    }, [url])
+    }, [url]);
 
     return {
         data,
         isPending,
-        error
+        error,
+    };
+}
+
+/**
+ * Represents an inmutable HTTP remote fetch error.
+ * It could act as base class of other more specific.
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#custom_error_types
+ */
+export class FetchError extends Error {
+    // as private fields (encapsulation)
+    #status;
+    #statusText;
+
+    constructor({status, statusText}, ...params) {
+        // provide toString() message and
+        // pass remaining arguments (including vendor specific ones) to parent constructor
+        super(`E${status}: ${statusText}`, ...params);
+
+        // Maintains proper stack trace for where our error was thrown (only available on V8)
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, CustomError);
+        }
+
+        this.#status = status;
+        this.#statusText = statusText;
+    }
+
+    /**
+     * The inmutable name error itself with extensibility support.
+     * ex. Eg in this context: `FetchError`.
+     * @return string
+     */
+    get name() {
+        return this.constructor.name;
+    }
+
+    /**
+     * Converts this class instances to JSON string.
+     * `JSON.stringify()` calls this mixin when marshalling to JSON.
+     * @return string
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#tojson_behavior
+     */
+    toJSON(/*key*/) {
+      // start with an empty object
+      const jsonObj = {};
+// or emit all properties and all fields
+//      const jsonObj = Object.assign({}, this);
+
+      // add all getter properties
+      const proto = Object.getPrototypeOf(this);
+      for (const key of Object.getOwnPropertyNames(proto)) {      
+          const descriptor = Object.getOwnPropertyDescriptor(proto, key);
+          const hasGetter = descriptor && typeof descriptor.get === 'function';
+          if (hasGetter) {
+              jsonObj[key] = desc.get();
+          }
+       }
+
+       return jsonObj;
+    }
+
+    /**
+     * An exportable JSON mark. Always true.
+     * @return bool
+     */
+    get error() {
+       return true;
+    }
+
+    /**
+     * Property with the HTTP response status code.
+     * @return number
+     */
+    get status() {
+        return this.#status;
+    }
+
+    /**
+     * Property with the HTTP response status message.
+     * @return number
+     */
+    get statusText() {
+        return this.#statusText;
     }
 
 }
