eb30b942a01a36b1bb4d8a5b1966054f70404520
Angel-Arceo <aalberto1812@gmail.com>
2021-08-26 20:58:09 -0500

    adding animations to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import './App.css'
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 import Home from './pages/Home/Home';
 import Recipe from './pages/Recipe/Recipe';
 
 const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route  path="/recipe" component={Recipe} />
-      </Switch>
+    <Router basename={process.env.PUBLIC_URL} >
+      <Route render={({ location }) => (
+        <TransitionGroup className="container">
+          <CSSTransition appear={true} key={location.key} timeout={{ enter: 400, exit: 200 }} classNames="fade">
+            <div className="inner">
+              <Switch key={location.key} location={location}>
+                <Route exact path="/" component={Home} />
+                <Route path="/recipe" component={Recipe} />
+              </Switch>
+            </div>
+          </CSSTransition>
+        </TransitionGroup>
+      )} />
     </Router>
   );
 }
