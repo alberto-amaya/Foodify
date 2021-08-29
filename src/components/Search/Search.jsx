b2847f2377659bea2d459c5639a6e1fca054fd05
Angel Arceo <aalberto1812@gmail.com>
2021-08-29 18:50:55 -0500

    changing the styles of the Search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -6,14 +6,18 @@ const Search = (props) => {
 
     const handleClick = () => {
         props.onSearch(inputSearch.current.value);
-        
     }
-    const bgStyle={
-        backgroundImage: `url(${process.env.PUBLIC_URL}/assets/icons/search.svg)`,
-    }
+
     return (
         <Fragment>
-            <input style={bgStyle} type="text" placeholder="Search" onChange={handleClick} ref={inputSearch} />
+            <button onClick={handleClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="10" cy="10" r="7" />
+                    <line x1="21" y1="21" x2="15" y2="15" />
+                </svg>
+            </button>
+            <input type="text" placeholder="Search" ref={inputSearch} />
         </Fragment>
     );
 }
