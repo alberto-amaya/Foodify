0ff437e854cc2f87b1be2ba652a505ecd6849af0
Luis Calderon <luis.calderon.lc2019@gmail.com>
2021-08-30 16:14:40 -0600

    refactor: change svg tags by Icon component

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import './Filters.css';
-
+import Icon from "../iconComponent/Icon";
 
 
 const Filters = () => {
@@ -8,18 +8,7 @@ const Filters = () => {
 
     return (
         <button className="filters" ref={filterButton}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-adjustments-horizontal" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <circle cx="14" cy="6" r="2" />
-                <line x1="4" y1="6" x2="12" y2="6" />
-                <line x1="16" y1="6" x2="20" y2="6" />
-                <circle cx="8" cy="12" r="2" />
-                <line x1="4" y1="12" x2="6" y2="12" />
-                <line x1="10" y1="12" x2="20" y2="12" />
-                <circle cx="17" cy="18" r="2" />
-                <line x1="4" y1="18" x2="15" y2="18" />
-                <line x1="19" y1="18" x2="20" y2="18" />
-            </svg>
+            <Icon name="filter" ></Icon>
         </button>
     )
 }
