2895fbd481c39a001db9ef5af3fa000efbbaca28
Angel Arceo <aalberto1812@gmail.com>
2021-09-01 15:51:18 -0500

    Uploading the filter component

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './Filter.css';
+
+const Filter = () => {
+    return (
+        <Fragment>
+            This is a filter
+        </Fragment>
+    );
+}
+
+export default Filter;
--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,7 +1,9 @@
 import React, { useState, Fragment } from 'react';
 import './Filters.css';
-import Icon from "../iconComponent/Icon";
+
+import Icon from '../iconComponent/Icon';
 import Modal from '../modal/Modal';
+import Filter from '../filter/Filter';
 
 const Filters = () => {
     const [ active, setActive ] = useState(false);
@@ -10,14 +12,27 @@ const Filters = () => {
         setActive(!active);
     }
 
+    /* 
+        maxReadyTime -> number 20 The maximum time in minutes it should take to prepare and cook the recipe.
+        minCalories	-> number 50 The minimum amount of calories the recipe must have.
+        maxCalories -> number 800 The maximum amount of calories the recipe can have.
+        minFat -> number 1 The minimum amount of fat in grams the recipe must have.
+        maxFat -> number 100 The maximum amount of fat in grams the recipe can have.
+        minCarbs -> number	10	The minimum amount of carbohydrates in grams the recipe must have.
+        maxCarbs -> number	100	The maximum amount of carbohydrates in grams the recipe can have.
+        number -> number	10	The number of expected results (between 1 and 100).
+    */
+
     return (
         <Fragment>
-            <button className="filters" onClick={toggle}>
+            <button className="filtersBtn" onClick={toggle}>
                 <Icon name="filter" width="25" height="25" fill="transparent" stroke="#fff"></Icon>
             </button>
 
             <Modal active={active} toggle={toggle} title="Filters" height="330">
-                <p>This is a filter</p>
+                <div className="filters">
+                    <Filter />
+                </div>
             </Modal>
         </Fragment>
     )
